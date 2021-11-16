3a72cc786cbc1bee6f3a33b850f62d8a6bbc16ab
jyturrieta <63125197+jyturrieta@users.noreply.github.com>
2021-11-15 22:09:13 -0300

    Corregido error del buscador

--- final/src/components/Buscador.jsx
+++ final/src/components/Buscador.jsx
@@ -8,7 +8,7 @@ const Buscador = props => {
         <div>
             <form onSubmit={props.consultarAPI}>
                 <div className="d-flex justify-content-center flex-column p-2 m-2 ">
-                    <input type="text" name="ciudad" placeholder="Ingrese una ciudad" className="rounded p-3" >
+                    <input type="text" name="ciudad" placeholder="Ingrese una ciudad" className="rounded p-3 buscador" >
                     </input>
                     <button className="btn  ">Buscar</button>
                 </div>
