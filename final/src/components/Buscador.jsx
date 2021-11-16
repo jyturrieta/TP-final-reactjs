004844803af7fe006a822876934736dc60714d92
jyturrieta <63125197+jyturrieta@users.noreply.github.com>
2021-11-15 21:23:32 -0300

    Update Buscador.jsx

--- final/src/components/Buscador.jsx
+++ final/src/components/Buscador.jsx
@@ -5,7 +5,7 @@ import "../index.css"
 const Buscador = props => {
     return ( 
 
-        <div className="">
+        <div>
             <form onSubmit={props.consultarAPI}>
                 <div className="d-flex justify-content-center flex-column p-2 m-2 ">
                     <input type="text" name="ciudad" placeholder="Ingrese una ciudad" className="rounded p-3 buscador" >
